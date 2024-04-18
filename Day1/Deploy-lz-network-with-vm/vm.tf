e22cfcb302a1d3cbe66f86a43fdcad047c50540b
Fredrik Wikestad <fredrik.wikestad@atea.no>
2024-04-18 09:41:02 +0200

    Update task 3

--- Day1/Deploy-lz-network-with-vm/vm.tf
+++ Day1/Deploy-lz-network-with-vm/vm.tf
@@ -1,9 +1,32 @@
-  module "windowsservers" {
-    source              = "Azure/vm/azurerm"
-    location            = "norwayeast"
-    vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
-    admin_password      = "ComplxP@ssw0rd!"
-    vm_os_simple        = "WindowsServer"
-    public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
-    vnet_subnet_id      = "${module.vnet.vnet_subnets[0]}"
-  }
+resource "azurerm_resource_group" "vm" {
+  name     = "rg-test"
+  location = var.resource_location
+}
+
+module "vm" {
+  source              = "github.com/fwikestad/terraform-azurerm-windows-vm"
+  vm_name             = "test"
+  resource_group_name = azurerm_resource_group.vm.name
+  location            = var.resource_location
+  subnet_id           = module.network.subnets["snet01"].id
+  vm_password         = "ChangeMe123!"
+  use_public_ip       = true
+}
+
+resource "azurerm_network_security_rule" "rdp" {
+  name                        = "Allow RDP From my IP"
+  resource_group_name         = module.network.resource_group_name
+  network_security_group_name = module.network.nsg_name
+
+  priority  = 100
+  direction = "Inbound"
+  access    = "Allow"
+  protocol  = "Tcp"
+
+  source_address_prefix = "[your public ip]"
+  source_port_range     = "*"
+
+  destination_address_prefix = "*"
+  destination_port_range     = "3389"
+
+}
